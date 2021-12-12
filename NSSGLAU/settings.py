434add84938f0f63bb76651ab0a4ad7bfb8b604f
Praful Sharma <praful.sharma_cs18@gla.ac.in>
2021-12-12 21:50:16 +0530

    change Database to sqllite

--- NSSGLAU/settings.py
+++ NSSGLAU/settings.py
@@ -79,20 +79,20 @@ WSGI_APPLICATION = 'NSSGLAU.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
     # We're using PostgreSQL database server for our Web Application.
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'NSSGLAU',
-        'USER': 'postgres',
-        'PASSWORD': 'qwerty',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'NSSGLAU',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'qwerty',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
 }
 
 
