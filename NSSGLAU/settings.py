27bb708d9e952bff2a46088fb30d4b122ec95c06
Praful Sharma <praful.sharma_cs18@gla.ac.in>
2021-12-12 21:43:40 +0530

    added whiltenoise url in middlewares.

--- NSSGLAU/settings.py
+++ NSSGLAU/settings.py
@@ -49,6 +49,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'NSSGLAU.urls'
