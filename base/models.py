1e878fb304410d25b7b3c84ee95ab45a8a79292c
Praful Sharma <praful.sharma_cs18@gla.ac.in>
2022-01-06 10:50:56 +0530

    alert message improvements.

--- base/models.py
+++ base/models.py
@@ -4,11 +4,11 @@ from django.db import models
 # Create your models here.
 
 class Contact(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
     date = models.DateTimeField(verbose_name='date_created', auto_now_add=True)
-    subject = models.CharField(max_length=150)
-    email = models.EmailField(verbose_name="email", max_length=60, unique=True)
-    message = models.TextField(max_length=500)
+    subject = models.TextField(max_length=300)
+    email = models.EmailField(verbose_name="email", max_length=100, unique=True)
+    message = models.TextField(max_length=1000)
 
     def __str__(self):
         return self.name
