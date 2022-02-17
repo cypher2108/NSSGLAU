634827c2ed21a506562e120a47ec276c99845978
Praful Sharma <praful.sharma_cs18@gla.ac.in>
2022-02-17 11:00:14 +0530

    added login page with mobile responsiveness

--- static/js/main.js
+++ static/js/main.js
@@ -8,3 +8,13 @@ console.log("js is working fine.");
 // document.querySelector(".popup .close-btn").addEventListener("click", function () {
 //     document.querySelector(".popup").classList.remove("active");
 // })
+
+$(function () {
+	$('.btn-link[aria-expanded="true"]').closest('.accordion-item').addClass('active');
+	$('.collapse').on('show.bs.collapse', function () {
+		$(this).closest('.accordion-item').addClass('active');
+	});
+	$('.collapse').on('hidden.bs.collapse', function () {
+		$(this).closest('.accordion-item').removeClass('active');
+	});
+});
